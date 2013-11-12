42702ea6ffeb694a74200989a3b3ddfbf5787d16
eleanormark <eleanormark@gmail.com>
2013-11-12 15:23:39 -0800

    fix spaces

--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -27,7 +27,7 @@ describe "translate" do
     translate("three").should == "eethray"
   end
 
-    it "counts 'sch' as a single phoneme" do
+  it "counts 'sch' as a single phoneme" do
     s = translate("school")
     s.should == "oolschay"
   end
@@ -42,32 +42,32 @@ describe "translate" do
     s.should == "ethay ickquay ownbray oxfay"
   end
 
-    it "translates a capitalized simple word beginning with a vowel" do
+  it "translates a capitalized simple word beginning with a vowel" do
     s = translate("Apple")
     s.should == "Appleay"
   end
 
-    it "translates a capitalized word beginning with a consonant" do
+  it "translates a capitalized word beginning with a consonant" do
     s = translate("School")
     s.should == "Oolschay"
   end
 
-    it "translates many capitalized words" do
+  it "translates many capitalized words" do
     s = translate("The Quick Brown Fox")
     s.should == "Ethay Ickquay Ownbray Oxfay"
   end
 
-    it "translates and retain '!'" do
+  it "translates and retain '!'" do
     s = translate("Apple!")
     s.should == "Appleay!"
   end
 
-   it "translates two words with '!!!'" do
+  it "translates two words with '!!!'" do
     s = translate("Eat pie!!!")
     s.should == "Eatay iepay!!!"
   end
         
-    it "translates many capitalized words and retain punctuations" do
+  it "translates many capitalized words and retain punctuations" do
     s = translate("The Quick Brown Fox eats pie, apple and? Eat pie.")
     s.should == "Ethay Ickquay Ownbray Oxfay eatsay iepay, appleay anday? Eatay iepay."
   end
